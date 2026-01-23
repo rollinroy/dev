fe2635f1a732e2f8a3158d429ebd012f2811417f
rollinroy <rollinforall@gmail.com>
2026-01-23 00:29:33 -0800

    Use linewidth instead of size

--- R_docker/R-performance/ggplot_stuff.R
+++ R_docker/R-performance/ggplot_stuff.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 gg <- ggplot(midwest, aes(x=area, y=poptotal)) +
   geom_point(aes(col=state), size=3) +  # Set color to vary based on state categories.
-  geom_smooth(method="lm", col="firebrick", size=2) +
+  geom_smooth(method="lm", col="firebrick", linewidth=4) +
   coord_cartesian(xlim=c(0, 0.1), ylim=c(0, 1000000)) +
   labs(title="Area Vs Population", subtitle="From midwest dataset", y="Population", x="Area", caption="Midwest Demographics")
 plot(gg)
